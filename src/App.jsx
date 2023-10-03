c9e02c51e613ab77dee90e1aa5c109e84a11774c
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-10-03 12:41:12 +0300

    08 React Router Dom | Routes in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,22 @@
-import Axios from 'axios'
-import { useEffect, useState } from 'react'
-import AgePredict from './components/AgePredict'
-import Excuser from './components/Excuser'
-
-//fetch function returns a promise
-/* fetch('https://catfact.ninja/fact')
-  .then(res => res.json())
-  .then(data => console.log(data)) */
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Menu } from './pages/Menu'
+import { Contact } from './pages/Contact'
+import { Navbar } from './Navbar'
 
 const App = () => {
-  const [catFact, setCatFact] = useState()
-
-  useEffect(() => {
-    fetchCatFact()
-  }, [])
-
-  const fetchCatFact = () => {
-    Axios.get('https://catfact.ninja/fact')
-    .then(res => {
-      setCatFact(res.data.fact)
-    })
-  }
 
   return (
     <div className="App">
-      <div>
-        <div><b>Interesting about cats:</b> <i>{catFact}</i></div>
-        <button onClick={fetchCatFact}>Get cat fact</button>
-        <p></p>
-      </div>
-      <AgePredict />
-      <Excuser />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router>
     </div>
   )
 }
